31553532d815f5e70d7db811c634c5605f53082a
Karthik Mittal <karthikmittal306@gmail.com>
2022-01-22 07:22:32 -0800

    Update test.R

--- test.R
+++ test.R
@@ -3,7 +3,7 @@ library(randomForest)
 library(car)
 
 # Example of body mass index on coronary heart diseases
-# Extract and harmonise data
+# Extract and harmonise data 
 a <- extract_instruments(2) 
 b <- extract_outcome_data(a$SNP, 7)
 dat <- harmonise_data(a,b)
